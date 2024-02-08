249ff02b472a0e02f291f86b2015edc66e7f18a1
ArkodeepC <arkodeep3404@gmail.com>
2024-02-08 18:38:15 +0530

    frontend parts added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import AddTodo from "./components/addTodo.jsx"
-import DisplayTodo from "./components/displayTodo.jsx"
 import { RecoilRoot } from "recoil"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/signup";
+import Signin from "./pages/signin";
+import Home from "./pages/home";
 
 export default function App() {
   return (
     <RecoilRoot>
-      <AddTodo />
-      <DisplayTodo />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </RecoilRoot>
   )
 }
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+import AddTodo from "../components/addTodo"
+import DisplayTodo from "../components/displayTodo"
+
+export default function Home() {
+  return (
+    <>
+    <AddTodo />
+    <DisplayTodo />
+    </>
+)};
