c0c6f6a3a6cae2dfbb81ebf5f4777418f68ddff9
ArkodeepC <arkodeep3404@gmail.com>
2024-02-29 13:35:13 +0530

    update dbschema.js

--- backend/dbSchema.js
+++ backend/dbSchema.js
@@ -1,27 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://arkodeep3404:v1iStwhqZTwhAInE@todo.bbmunuu.mongodb.net');
+mongoose.connect(
+  "mongodb+srv://arkodeep3404:v1iStwhqZTwhAInE@todo.bbmunuu.mongodb.net"
+);
 
 const schema = mongoose.Schema;
-const model = mongoose.model
+const model = mongoose.model;
 
 const userSchema = new schema({
-    email: String,
-    password: String,
-    firstName: String,
-    lastName: String
+  email: String,
+  password: String,
+  firstName: String,
+  lastName: String,
 });
 
 const todoSchema = new schema({
-    userId: schema.Types.ObjectId,
-    title: String,
-    description: String
+  userId: schema.Types.ObjectId,
+  title: String,
+  description: String,
 });
 
-const User = model('User', userSchema);
-const Todo = model('Todo', todoSchema);
+const User = model("User", userSchema);
+const Todo = model("Todo", todoSchema);
 
 module.exports = {
-    User,
-    Todo
-}
+  User,
+  Todo,
+};
